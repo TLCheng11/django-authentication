181786d1dea517f0b1c36b7a949e98ccfc67b740
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-10-06 23:21:50 -0400

    add Post model and migration

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 
 
 # Create your views here.
+# To redirect if user is not login
+@login_required(login_url="/login")
 def home(request):
     return render(request, 'main/home.html')
 
