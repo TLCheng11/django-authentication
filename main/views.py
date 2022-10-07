3d29382c65213d7bf2039762cee76e850bc2c3f1
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-10-07 16:12:35 -0400

    show all posts at homepage

--- main/views.py
+++ main/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm, RegistrationForm
+from .models import Post
 
 
 # Create your views here.
 # To redirect if user is not login
 @login_required(login_url="/login")
 def home(request):
-    return render(request, 'main/home.html')
+    posts = Post.objects.all()
+    return render(request, 'main/home.html', {"posts": posts})
 
 def sign_up(request):
     if request.method == "POST":
